8eea445b8d15e446aaa1823e2a86f20967769d73
cristinashuw <cristinashuw@gmail.com>
2020-08-06 18:28:58 +0700

    MainActivity

--- app/src/main/java/com/example/retrofit_2nd/MainActivity.java
+++ app/src/main/java/com/example/retrofit_2nd/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.retrofit_2nd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.example.retrofit_2nd.pojo.MultipleResource;
+import com.example.retrofit_2nd.pojo.User;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+    TextView responseText;
+    APIInterface apiInterface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        responseText = (TextView) findViewById(R.id.responseText);
+        apiInterface = APIClient.getClient().create(APIInterface.class);
+
+
     }
 }
