56cf4a6c27d27ba7c42c3aa3acacc5f2d16d60d7
cristinashuw <cristinashuw@gmail.com>
2020-08-06 18:22:47 +0700

    @SerializedName

--- app/src/main/java/com/example/retrofit_2nd/pojo/CreateUserResponse.java
+++ app/src/main/java/com/example/retrofit_2nd/pojo/CreateUserResponse.java
@@ -1,4 +1,14 @@
 package com.example.retrofit_2nd.pojo;
 
+import com.google.gson.annotations.SerializedName;
+
 public class CreateUserResponse {
+    @SerializedName("name")
+    public String name;
+    @SerializedName("job")
+    public String job;
+    @SerializedName("id")
+    public String id;
+    @SerializedName("createdAt")
+    public String createdAt;
 }
